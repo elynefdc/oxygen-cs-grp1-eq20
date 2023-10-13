e2bca117f62b958a8e978960108e73b245511863
elynefdc <elyne.fdc1@gmail.com>
2023-10-13 17:23:42 -0400

    ajouts varibables environnement

---
+++ src/variables_environnement.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ["HOST"] = "https://hvac-simulator-a23-y2kpq.ondigitalocean.app"
+os.environ["TOKEN"] = "t3ivYx3wZ5"
+os.environ["T_MAX"] = "25"
+os.environ["T_MIN"] = "10"
+os.environ["DATABASE"] = "BDD"
+
+print("Variables d'environnement configurées avec succès.")
