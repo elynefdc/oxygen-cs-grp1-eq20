2c74400cf4770eda2addaee87b1cbe037d81e791
elynefdc <elyne.fdc1@gmail.com>
2023-10-27 09:48:55 -0400

    ajout test unitaire precommit

--- test/test.py
+++ test/test.py
@@ -1,1 +1,14 @@
-## To Implement
+import unittest
+
+class TestMethods(unittest.TestCase):
+
+    self.T_MAX = 25
+    self.T_MIN = 15
+
+    def testMaxIsNotMin():
+        assert T_MAX >= T_MIN
+
+
+
+if name == 'main':
+    unittest.main()
