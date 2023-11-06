105637005850c8e49f7e41c70844d3c3c893a674
elynefdc <144647580+elynefdc@users.noreply.github.com>
2023-11-06 03:17:44 -0500

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,11 @@
 import pytest
-
+from dotenv import load_dotenv
 from src.main import Main
+
+load_dotenv()
 
 # Tests
 def test_environment_variables():
     
-    assert 1  == 1
+    assert T_MAX  == 1
     
