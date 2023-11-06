258a34bffaf27b2bf324deafc3e17ca9658325b1
elynefdc <144647580+elynefdc@users.noreply.github.com>
2023-11-05 19:30:49 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from datetime import datetime, timezone
-from db_map import UserEvent
+from .db_map import UserEvent
 from db_map import SystemEvent
 from db_map import Base
 
