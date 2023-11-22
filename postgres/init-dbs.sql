beea4900ea308f80796b69aa2c89ce43b32aa258
Jean Lamarre <jonny.lamarre@hotmail.com>
2023-11-22 17:56:24 -0500

    test db create

--- postgres/init-dbs.sql
+++ postgres/init-dbs.sql
@@ -1,5 +1,2 @@
-SELECT 'CREATE DATABASE db_oxygen'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db_oxygen')\gexec ;
-
-SELECT 'CREATE DATABASE db_postgres'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db_postgres')\gexec;
+CREATE DATABASE db_oxygen;
+CREATE DATABASE db_postgres;
